11943279416bb7d5caaa3fba465abbb575d913d3
kis-dr <AS9999@koreainvestment.com>
2026-06-23 14:39:00 +0900

    update

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d C:\python\kis_digital_ra\contract-timeline
-git add .
-if "%~1"=="" (
-    git commit -m "update"
-) else (
-    git commit -m "%~1"
-)
-git push
--- run_venv - contract.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call C:\python\python3_12_TA\.venv\Scripts\activate.bat
-cd C:\python\kis_digital_ra\contract-timeline\pipeline
-python build_data.py --backfill 2026-04-30 2026-04-30
-pause
